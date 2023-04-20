76c9e6403a60adf055f070574c6f21c886c8b5a0
João Victor <jvfernandes.contato@gmail.com>
2023-04-20 16:11:32 -0300

    feat: making items dropable

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 const dragItems = document.querySelectorAll('.drag')
 const dropBoxes = document.querySelectorAll('.box-space-droppable')
 
+// Drag Events
 dragItems.forEach(item => {
   item.addEventListener('dragstart', dragStart)
 });
 
-function dragStart() {
+//Drop Events
+dropBoxes.forEach(box => {
+  box.addEventListener('dragover', dragOver);
+  box.addEventListener('drop', dropEvent);
+  box.addEventListener('dragleave', dragLeave);
+});
+
+function dragStart(e) {
+  e.dataTransfer.setData('text/plain', e.target.innerText)
+
   setTimeout(() => {
     this.className = 'invisible'
   }, 0)
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  
+  this.className += ' enter'
+}
+
+function dropEvent(e) {
+  e.preventDefault();
+  
+  this.className = 'box-space-droppable'
+
+  const el = document.createElement('p');
+  el.className = 'drag';
+  el.innerText = e.dataTransfer.getData('text');
+
+  this.appendChild(el)
+}
+
+function dragLeave(e) {
+  e.preventDefault();
+
+  this.className = 'box-space-droppable'
 }
